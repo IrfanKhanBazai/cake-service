02873585d4db0a8194a2dcc07b8bc1e6c4ee1339
irfan khan <irfankhanbazai@yahoo.com>
2018-04-07 18:05:15 +0100

    commiting new files

--- src/main/java/com/waracle/Application.java
+++ src/main/java/com/waracle/Application.java
@@ -18,7 +18,7 @@ public class Application {
 
 	@Bean
 	public CommandLineRunner demo(InitialDataLoader dataLoader) {
-		return (args) -> {		
+		return (args) -> {
 			String jsonCakeUrl ="https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/raw/8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json";
 			dataLoader.loadsJsonCakeDataFromURL(jsonCakeUrl);
 		};
